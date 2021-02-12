fc0d27c69b40a2c9c47fc3d3b969cd81676f333b
Andre <andreguilhermehigino@gmail.com>
2021-02-11 23:27:50 -0400

    Add apresentção

---
+++ app/src/main/java/com/priscila/desafioeldorado/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.priscila.desafioeldorado
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 5000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        },   SPLASH_TIME_OUT )
+
+    }
+}
